5e6f3ec887da16db5d5895279f225a0fcb75dcc2
Naeem_Upgrade_PC <naeemgg@duck.com>
2022-04-14 17:27:27 +0530

    registration data not writing bug fixed

--- login.php
+++ login.php
@@ -10,6 +10,17 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <style>
+        body
+        {
+            background-image: linear-gradient(135deg, #FAB2FF 10%,#1904E5 100%);
+            background-size:cover;
+            background-repeat:no-repeat;
+            background-attachment:fixed;
+            font-family:"Open Sans",sans-serif;
+            color:#333333;
+            
+        }
+
         .gradient-custom-2 {
             /* fallback for old browsers */
             background: #fccb90;
@@ -20,6 +31,14 @@
             /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
             background: linear-gradient(to right, #ee7724, #d8363a, #dd3675, #b44593);
         }
+        /* #left
+        {
+            color:black;
+            background-size:cover;
+            background-repeat:no-repeat;
+            background-image: url("/images/logpage2.jpg");
+            overflow:hidden;
+        } */
 
         @media (min-width: 768px) {
             .gradient-form {
@@ -59,6 +78,7 @@
             /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
             background: linear-gradient(to right, rgba(132, 250, 176, 1), rgba(143, 211, 244, 1))
         }
+        
     </style>
     <title>Login Page</title>
 </head>
@@ -73,7 +93,7 @@
                 <div class="col-xl-10">
                     <div class="card rounded-3 text-black">
                         <div class="row g-0">
-                            <div class="col-lg-6">
+                            <div class="col-lg-6" id="left">
                                 <div class="card-body p-md-5 mx-md-4">
 
                                     <div class="text-center">
@@ -115,19 +135,22 @@
 
                                 </div>
                             </div>
-                            <div class="col-lg-6 d-flex align-items-center gradient-custom-2">
-                                <div class="text-white px-3 py-4 p-md-5 mx-md-4">
-                                    <h4 class="mb-4">We're more than just a Web app </h4>
-                                    <p class="small mb-0">A web-based application is any application that uses a website
-                                        as the interface or front-end. Clients can without much of a stretch access the
-                                        application from any PC associated with the Internet utilizing a standard
-                                        program. It merits calling attention to that practically any work area
-                                        programming can be created as a web-based application. Web applications go
-                                        through web programs like Google Chrome. The program runs on a web server, as
-                                        opposed to on the PC, or nearby server for customary applications.</p>
+        
+
+                                <div class="col-lg-6 d-flex align-items-center gradient-custom-2">
+                                    <div class="text-white px-3 py-4 p-md-5 mx-md-4">
+                                        <h4 class="mb-4">We're more than just a Web app </h4>
+                                        <p class="small mb-0">A web-based application is any application that uses a website
+                                            as the interface or front-end. Clients can without much of a stretch access the
+                                            application from any PC associated with the Internet utilizing a standard
+                                            program. It merits calling attention to that practically any work area
+                                            programming can be created as a web-based application. Web applications go
+                                            through web programs like Google Chrome. The program runs on a web server, as
+                                            opposed to on the PC, or nearby server for customary applications.</p>
+                                        </div>
+                                    </div>
                                 </div>
-                            </div>
-                        </div>
+        
                     </div>
                 </div>
             </div>
