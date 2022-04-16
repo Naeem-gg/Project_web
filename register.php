5e76d016549c3b15dcfa7db5ef3d02c3ff296bd6
Naeemgg <naeemgg@duck.com>
2022-04-16 22:29:47 +0530

    password matching feature added without JS by usung PHP

--- register.php
+++ register.php
@@ -7,8 +7,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1">
 
   <!-- Bootstrap CSS -->
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
-    integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
   <style>
     .gradient-custom-3 {
       /* fallback for old browsers */
@@ -31,27 +30,18 @@
       /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
       background: linear-gradient(to right, rgba(132, 250, 176, 1), rgba(143, 211, 244, 1))
     }
-    #sta
+
+    span[name='sta'] 
     {
-      color:white;
+      color: white;
     }
   </style>
-  <script>
-      var pass=document.getElementsByName('pass');
-      var pass2=document.getElementsByName('pass2');
-      if(pass!==pass2)
-      {
-        // document.getElementById('sta').innerHTML="Password doesn't match";
-        // document.getElementById('sta').style.color="red";
-        alert("password doen't match");
-      }
-  </script>
+
   <title>New Register</title>
 </head>
 
 <body>
-  <section class="vh-100 bg-image"
-    style="background-image: url('https://mdbcdn.b-cdn.net/img/Photos/new-templates/search-box/img4.webp');">
+  <section class="vh-100 bg-image" style="background-image: url('https://mdbcdn.b-cdn.net/img/Photos/new-templates/search-box/img4.webp');">
     <div class="mask d-flex align-items-center h-100 gradient-custom-3">
       <div class="container h-100">
         <div class="row d-flex justify-content-center align-items-center h-100">
@@ -59,10 +49,10 @@
             <div class="card" style="border-radius: 15px;">
               <div class="card-body p-5">
                 <h2 class="text-uppercase text-center mb-5">Create an account</h2>
-                
-                
 
-                <form action="<?php echo $_SERVER['PHP_SELF'];?>" method="POST">
+
+
+                <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="POST">
 
                   <div class="form-outline mb-4">
                     <label class="form-label" for="form3Example1cg">Your Name:</label>
@@ -75,23 +65,37 @@
                     <small>Choose a username</small>
                   </div>
                   <div class="form-outline mb-4">
+                    <label class="form-label" for="form3Example1cg"> Phone Number:</label>
+                    <input type="text" id="form3Example1cg" class="form-control form-control-lg" name="phone" />
+                    <small>Enter 10 digit number without country code</small>
+                  </div>
+                  <div class="form-outline mb-4">
                     <label class="form-label" for="form3Example3cg">Your Email</label>
-                    <input type="email" id="form3Example3cg" class="form-control form-control-lg"  name="email" />
+                    <input type="email" id="form3Example3cg" class="form-control form-control-lg" name="email" />
                     <small>example@gmail.com</small>
                   </div>
 
                   <div class="form-outline mb-4">
                     <label class="form-label" for="form3Example4cg">Password</label>
-                    <input type="password" id="form3Example4cg" class="form-control form-control-lg"  name="pass" />
+                    <input type="password" id="form3Example4cg" class="form-control form-control-lg" name="pass" />
                   </div>
 
                   <div class="form-outline mb-4">
-                    <label class="form-label" for="form3Example4cdg">Repeat your password</label>
+                    <label class="form-label" for="form3Example4cdg">Confirm password</label>
                     <input type="password" id="form3Example4cdg" class="form-control form-control-lg" name="pass2" />
                   </div>
-                      <div id="sta">
-                          *
-                      </div>          
+                  <div>
+                    <span name="sta">*</span>
+                  </div>
+                  <div class="btn-group btn-group-toggle" data-toggle="buttons">
+                    <label class="btn btn-secondary active">
+                      <input type="radio" name="options" id="option1" autocomplete="off" checked> Male
+                    </label>
+                    <label class="btn btn-secondary">
+                      <input type="radio" name="options" id="option2" autocomplete="off"> Female
+                    </label>
+
+                  </div><br><br>
                   <div class="form-check d-flex justify-content-center mb-5">
                     <input class="form-check-input me-2" type="checkbox" value="" id="form2Example3cg" />
                     <label class="form-check-label" for="form2Example3g">
@@ -99,50 +103,60 @@
                     </label>
                   </div>
                   <p>
-                  <?php
-                  if(isset($_POST['sub']))
-                  {
+                    <?php
+                    if (isset($_POST['sub'])) {
 
-                      $user=$_POST['user'];
-                      $pass=$_POST['pass'];
-                      $email=$_POST['name'];
-                      $name=$_POST['email'];
-                      $whole_data=array($name,$user,$email,$pass);
-                      $file=fopen("users.csv","r");
-                      while(($row=fgetcsv($file))!==false)
+                      $user = $_POST['user'];
+                      $pass = $_POST['pass'];
+                      $email = $_POST['name'];
+                      $name = $_POST['email'];
+                      $phone=$_POST['phone'];
+                      $gender = $_POST['options'];
+                      $whole_data = array($name, $user, $email, $pass, $gender);
+                      $file = fopen("users.csv", "r");
+                      while (($row = fgetcsv($file)) !== false) 
                       {
-                      if($row[0]===$user)
-                      {
+                        if ($row[0] === $user) 
+                        {
                           die("<div class='alert alert-danger' role='alert'>
                           Username '$user' is already exists. 
                           </div>");
+                        }
                       }
-                      
+                      if($pass!==$pass2)
+                      {
+                        die("<div class='alert alert-danger' role='alert'>
+                        Password doesn't match. 
+                        </div>");
+                        
+                        echo "<div class='alert alert-success' role='alert'>
+                        $user registration successfull!
+                        </div><a href=login.php> click here to login</a>";
+                        $fileappend = fopen("users.csv", "a");
+                        $data = array($_POST['user'], $_POST['pass']);
+                        fputcsv($fileappend, $data) or die("ERROR");
+                        //  sleep(0.5);
+                        $mainfile = fopen("registration_details.csv", "a");
+                        fputcsv($mainfile, $whole_data) or die("ERROR! unable to write data in $mainfile");
+                        fclose($file);
+                        fclose($fileappend);
+                        fclose($mainfile);
+                        //  header("Location:homepage.html");
                       }
-                      
-                          // echo "<div class='alert alert-success' role='alert'>
-                          // Login details saved successfully!
-                          // </div>";
-                          $fileappend=fopen("users.csv","a");
-                          $data=array($_POST['user'],$_POST['pass']);
-                          fputcsv($fileappend,$data) or die("ERROR");
-                          //  sleep(0.5);
-                          fclose($file);
-                          fclose($fileappend);
-                          //  header("Location:homepage.html");
-                  }
-                  ?>
-              </p> 
+                    }
+                    ?>
+                  </p>
+                 
                   <div class="d-flex justify-content-center">
-                    <input type="submit"
-                      class="btn btn-success btn-block btn-lg gradient-custom-4 text-body" value="Register" name="sub">
+                    <input type="submit" class="btn btn-success btn-block btn-lg gradient-custom-4 text-body" value="Register" name="sub">
                   </div>
 
-                  <p class="text-center text-muted mt-5 mb-0">Have already an account? <a href="login.php"
-                      class="fw-bold text-body"><u>Login here</u></a></p>
+                  <p class="text-center text-muted mt-5 mb-0">Have already an account? <a href="login.php" class="fw-bold text-body"><u>Login here</u></a></p>
 
                 </form>
-
+<script>
+  
+</script>
               </div>
             </div>
           </div>
@@ -154,9 +168,7 @@
   <!-- Optional JavaScript; choose one of the two! -->
 
   <!-- Option 1: Bootstrap Bundle with Popper -->
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
-    integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p"
-    crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 
   <!-- Option 2: Separate Popper and Bootstrap JS -->
   <!--
